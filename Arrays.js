4a19f3963f79353b8746ef4b96bd0cec37bb8be5
srinija landa <srinijalanda@gmail.com>
2024-05-20 22:40:42 +0530

    array-1

---
+++ Arrays.js
@@ -0,0 +1,43 @@
+/**
+ * Arrays in Javascript
+ * Compared to other Programming language 
+ * Arrays in JS categoried under the object Datatype.
+ * Arrays are the collection of data (different datatype HETEROGENOUS)
+ * Denoted by [] separated by commas
+ * follows the Indexing formate from 0 to n-1
+ * arr.length gives the Number of elements present in the array
+ * OutofBounde,negative indexing --->undefined
+ * Nested Indexing are allowed
+ * BULIT-IN METHODS
+ * ADD ->unshift(front),push(last)
+ * REMOVE ->shift(front) ,pop(last) -->returns the deleted values
+ * Add and remove anywhere splice(indexno,countNo to delete,element to insert); -->the deleted element in the form of ARRAY
+ * 
+ */
+
+/*
+
+let arr=[12,"srinija",true,[24,25,1],false,100];
+console.log(arr);
+
+//arr.length is a property
+console.log("the length of the array ",arr.length);
+
+//Accesing the elements from the array using the indexing
+console.log("accessing the elements from the array:",arr[4]);
+
+//Accessing the element that doesn't exist in the array -->undefined
+console.log("the outOfBoundElement",arr[7])
+
+//Negative Indexing
+console.log("the negative indexing",arr[-9]);
+
+// Nested Indexing are allowed forms the matrix
+
+let nested=[24,["sri","nija"],"sfc",25];
+console.log("the index at 2 is an arr",nested[1]);
+console.log(nested[1][0])
+
+*/
+
+//BULIT-IN FUNCTION OF ARRAY.
